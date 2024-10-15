b8e8352cb206f9edcb5254bc6842362690ba3e3e
MarceloMosl <marcelomosl@icloud.com>
2024-10-14 17:18:44 -0700

    feat: cleaning project structure

--- AdvancedTodoListApp/switcher.py
+++ AdvancedTodoListApp/switcher.py
@@ -1,9 +1,17 @@
+from functions.addTask import addTask
+from functions.viewTasks import viewTasks
+from functions.removeTask import removeTask
+from functions.suggestTask import suggestTask
+
+
 def switch(value, taskList: list):
     switcher = {
-        "1": lambda: print("Option 1"),  # add a task
-        "2": lambda: print("Option 2"),  # remove a task
-        "3": lambda: print("Option 3"),  # view tasks
-        "4": lambda: print("Option 4"),  # suggest tasks based on priority and deadlines
+        "1": lambda: addTask(taskList),  # add a task
+        "2": lambda: removeTask(taskList),  # remove a task
+        "3": lambda: viewTasks(taskList),  # view tasks
+        "4": lambda: suggestTask(
+            suggestTask
+        ),  # suggest tasks based on priority and deadlines
     }
     switcher.get(
         value, lambda: print("Invalid option, please choose a valid option.")
